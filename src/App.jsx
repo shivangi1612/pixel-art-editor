2f997ab802dff9f04ee36cb566d09bd6713bfb8d
shivangi1612 <shivangi.singh161204@gmail.com>
2025-02-26 16:16:26 +0530

    commitaaj

--- src/App.jsx
+++ src/App.jsx
@@ -67,7 +67,7 @@ function App() {
     <motion.div
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
-      className="min-h-screen bg-[url(/src/assets/image.png)] p-8"
+      className="min-h-screen bg-[url(/src/assets/image.jpg)] bg-auto md:bg-cover bg-no-repeat p-8"
     >
       <div className="max-w-7xl mx-auto ">
         <motion.h1
